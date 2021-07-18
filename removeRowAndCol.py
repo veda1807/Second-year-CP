2df7a7a0794ff75d4d6e1d8f74772669bf4d82f8
veda1807 <vedaravutla@msitprogram.net>
2021-07-19 00:42:54 +0530

    row nd col

---
+++ removeRowAndCol.py
@@ -0,0 +1,40 @@
+# removeRowAndCol (non-destructive and destructive)
+# Here we will write removeRowAndCol(row, col), 
+# Do not use copy.deepcopy and directly construct 
+# the modified 2d list.
+
+# The function takes a rectangular list L and two ints, 
+# row and col. the goal is to obtain a version of the list 
+# that has the given row and given column removed. 
+# You may assume that row and col are both legal values 
+# (that is, they are non-negative integers that are smaller 
+# than the largest row and column, respectively). For example, 
+# the list shown to the left would lead to the result shown on 
+# the right when called with row 1 and column 2.
+
+# list
+# [ [ 2, 3, 4, 5],
+#   [ 8, 7, 6, 5],
+#   [ 0, 1, 2, 3] ]
+
+# result
+# [ [ 2, 3, 5],
+#   [ 0, 1, 3] ]
+
+# import numpy as np
+
+def removeRowAndCol(L, row, col):
+    a=[]
+    if(len(L)==1 or len(L)==0 or row==0 or col==0) :
+        return "We can not delete thr row and col"
+    else:
+        for i in L:
+            del i[col]
+        del i[row]
+        return L
+
+# Write your own test cases.
+
+# a=[[0, 9, 2],
+#  [4, 5, 6],
+#  [8, 9, 7]]
