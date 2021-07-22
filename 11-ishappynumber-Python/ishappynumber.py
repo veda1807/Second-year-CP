80db1efee261e3d6943e6dcd84eabbd850e87a32
veda1807 <vedaravutla@msitprogram.net>
2021-07-22 12:35:49 +0530

    happy num

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -9,14 +9,24 @@
 
 # Write the function ishappynumber(n) which takes a possibly-negative integer and returns True if it is happy and 
 # False otherwise. Note that all numbers less than 1 are not happy. Here are some test assertions for you:
-# assert(ishappynumber(-7) == False)
-# assert(ishappynumber(1) == True)
-# assert(ishappynumber(2) == False)
-# assert(ishappynumber(97) == True)
-# assert(ishappynumber(98) == False)
-# assert(ishappynumber(404) == True)
-# assert(ishappynumber(405) == False)
 
 def ishappynumber(n):
-	# your code goes here
-	pass
+    # your code goes here  
+    if(n<0 or (n>1 and n<10)):     
+        return False
+    while(n>=10):
+        sum=0
+        for i in (str(n)):
+            sum=sum+(int(i)*int(i))
+        n=sum
+    if(n==1):
+        return True
+    else:
+        return False
+# print(ishappynumber(98))
+# print(ishappynumber(97))
+# print(ishappynumber(404))
+
+
+
+	
