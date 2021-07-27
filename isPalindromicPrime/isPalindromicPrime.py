a101a8f2c7ea56f9312707e553888194e61b470e
veda1807 <vedaravutla@msitprogram.net>
2021-07-27 19:11:35 +0530

    prime palindrome

--- isPalindromicPrime/isPalindromicPrime.py
+++ isPalindromicPrime/isPalindromicPrime.py
@@ -10,15 +10,19 @@ def isPrime(x):
         if(x%factor==0):
             return False
     return True
-def Palindrome(n):
-    
+# def isPalindrome(n):
+#     return (str(n)==str(n)[::-1])
+def isPalindromicPrime(n):
+    if(n==2):
+        return True
     if(n>9):
         n=str(n)
         if(len(n)%2==0):
+            n=int(n)
             a=n//2
+            n=str(n)
         else:
             a=len(n)//2+1
-        # n=str(n)
         j=len(n)-1
         for i in range(a):
             if(n[i]!=n[j]):
@@ -29,4 +33,38 @@ def Palindrome(n):
                 
         c=isPrime(a)
         return c
-# print(Palindrome(182))
+
+
+    # if(isPalindrome(n) and isPrime(n)):
+    #     return True
+    # return False
+
+# def isPalindromicPrime(n):
+    # num=n
+    # rev=0
+    # if(isPrime(n)):
+    #     while(n>0):
+    #         a=n%10
+    #         rev=rev*10+a
+    #         n=n//10
+    #     if(rev==num):
+    #         return True
+        
+    # return False
+
+
+
+    
+    
+
+
+# assert(isPalindromicPrime(2) == True)
+# print(assert(isPalindromicPrime(10) == False))
+# print(assert(isPalindromicPrime(104) == False))
+# print(assert(isPalindromicPrime(235) == False))
+# print(assert(isPalindromicPrime(131) == True))
+# assert (isPalindromicPrime(900) == False)
+# assert (isPalindromicPrime(101) == True)
+# assert (isPalindromicPrime(383) == True)
+# assert (isPalindromicPrime(373) == True)
+# assert (isPalindromicPrime(121) == False)
