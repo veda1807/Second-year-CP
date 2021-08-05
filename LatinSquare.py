e4c6273dac1f61613f446cadb239fbb669636e45
veda1807 <vedaravutla@msitprogram.net>
2021-08-06 02:30:39 +0530

    latin

--- LatinSquare.py
+++ LatinSquare.py
@@ -9,4 +9,17 @@
 
 def isLatinSquare(lst):
     # Your code goes here...
-    pass
+    for i in range(len(lst)):
+        # print(i)
+        if(lst.count(lst[i])>1):
+            return False
+        else:
+            for j in range(len(lst[i])):
+                if( (lst[i].count(lst[i][j])>1) or (lst[i].count(lst[j][i])>1) ):
+                    return False
+    return True
+print(isLatinSquare([
+    [1,2,3],
+    [2,3,1],
+    [3,1,2]
+]))
